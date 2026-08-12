a02f27b9af6a8e101bc62b3e190210e774036eaa
rnr <roehner.tom@gmail.com>
2026-08-12 23:05:00 +0200

    Eval before iteration 1

--- scripts/benchmarks/ppoewc_jaxatari_scan.py
+++ scripts/benchmarks/ppoewc_jaxatari_scan.py
@@ -634,7 +634,7 @@ def single_run(key: jax.random.PRNGKey, network: Network | MLP_Network, actor: A
     for iteration in range(1, config["NUM_ITERATIONS"] + 1):
         global_iteration += 1
         rtpt.step()
-        if config["EVAL_DURING_TRAIN"] and iteration > 0 and iteration % config["EVAL_EVERY"] == 0:
+        if config["EVAL_DURING_TRAIN"] and (iteration % config["EVAL_EVERY"] == 0 or iteration == 1):
            eval_and_vid(iteration, global_step) 
 
         iteration_time_start = time.time()
